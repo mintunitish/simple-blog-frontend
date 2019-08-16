bb52eaf02e6fca05e851b282748db22d275e6f27
Nitish Kumar <nitish.kumar@sugaldamani.com>
2019-08-16 12:25:49 +0530

    Initial Commit

---
+++ src/utils/helper.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019, Nitish Kumar <mintu.nitish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Created By: Nitish Kumar on 16/08/19 12:23 PM
+ */
+
+export const server = {
+    baseURL: 'http://localhost:3000',
+};
